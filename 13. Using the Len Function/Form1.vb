c2d744fca0781d8d1365ee7e2d212dab76e972c6
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-04 21:03:47 +0200

    Add project files.

---
+++ 13. Using the Len Function/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim MyText As String
+        MyText = "Visual Basic 2022"
+        MsgBox(Len(MyText))
+
+    End Sub
+End Class
